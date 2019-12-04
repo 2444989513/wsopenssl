f0c507ff7a6519cab3624ff28f5013b380b5d3ac
2444989513 <56295510+2444989513@users.noreply.github.com>
2019-12-04 09:58:03 +0800

    Create openssl.sh

---
+++ openssl.sh
@@ -0,0 +1,21 @@
+#安装依赖,同时上传软件包到服务器。
+yum -y install perl-core libtemplate-perl zlib-devel gcc wget
+#下载openssl
+wget https://www.openssl.org/source/openssl-1.1.1d.tar.gz
+#备份原openssl
+mv /usr/bin/openssl /usr/bin/openssl.bak
+#解压	
+tar zxvf openssl-1.1.1d.tar.gz
+#进入文件夹
+cd openssl-1.1.1d
+./config --prefix=/usr/local/ssl
+make&&make install
+#处理库文件
+echo "/usr/local/ssl/lib">>/etc/ld.so.conf
+ldconfig -v
+#处理命令文件
+ln -s /usr/local/ssl/bin/openssl /usr/bin/openssl
+ln -s /usr/local/ssl/include/openssl /usr/include/openssl
+#检查版本
+openssl version
+
